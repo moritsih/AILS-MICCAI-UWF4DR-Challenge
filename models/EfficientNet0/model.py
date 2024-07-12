3581ede128d45e7828c112af6fed41bb085b70b6
Berthold Scheuringer <berthold.scheuringer@gmail.com>
2024-07-12 17:31:29 +0200

    add missing color space conversion

--- models/EfficientNet0/model.py
+++ models/EfficientNet0/model.py
@@ -2,6 +2,7 @@ import os
 import torch
 from efficientnet_pytorch import EfficientNet
 from torchvision import transforms
+import cv2
 
 def remove_prefix(state_dict, prefix):
     """
@@ -50,6 +51,8 @@ class model:
         :param input_image: the input image to the model.
         :return: a float value indicating the probability of class 1.
         """
+        input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+
         # apply the same transformations as during validation
         transform = transforms.Compose([
             transforms.ToPILImage(),
