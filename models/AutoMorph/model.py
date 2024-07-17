cd58085011da94f1717736709d43d55c95146adc
Berthold Scheuringer <berthold.scheuringer@gmail.com>
2024-07-17 11:18:24 +0200

    adapted automorph submission model

--- models/AutoMorph/model.py
+++ models/AutoMorph/model.py
@@ -1,15 +1,16 @@
 import os
-import cv2
 import torch
 from torch import nn
 from efficientnet_pytorch import EfficientNet
+from torchvision import transforms
 
 
 class model:
     def __init__(self):
-        self.checkpoint = "automorph_best_loss_checkpoint.pth"
+        self.checkpoint = "automorph_best_model_2024-07-16_18-04-41.pth"
         # The model is evaluated using CPU, please do not change to GPU to avoid error reporting.
         self.device = torch.device("cpu")
+        self.model = None
 
     def init(self):
         pass  # nothing to do here
@@ -23,10 +24,14 @@ class model:
         :param dir_path: path to the submission directory (for internal use only).
         :return:
         """
-        self.model = AutoMorphModel()
+        self.model = build_automorph_model()
         # join paths
         checkpoint_path = os.path.join(dir_path, self.checkpoint)
-        self.model.load_state_dict(torch.load(checkpoint_path, map_location=self.device))
+
+        state_dict = torch.load(checkpoint_path, map_location=self.device)
+        state_dict = remove_prefix_in_state_dict(state_dict,'model.')  # we need to remove the prefix as on training EfficientNet was wrapped
+
+        self.model.load_state_dict(state_dict)
         self.model.to(self.device)
         self.model.eval()
 
@@ -41,36 +46,47 @@ class model:
         :param input_image: the input image to the model.
         :return: a float value indicating the probability of class 1.
         """
-        image = cv2.resize(input_image, (512, 512))
-        image = image / 255
-        image = torch.from_numpy(image).permute(2, 0, 1).unsqueeze(0)
-        image = image.to(self.device, torch.float)
+        # apply the same transformations as during validation
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(size=(800, 1016)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        image = transform(input_image)
+        image = image.unsqueeze(0)  # Add batch dimension
+        image = image.to(self.device)
 
         with torch.no_grad():
             output = self.model(image)
-            prob = torch.softmax(output, dim=1).squeeze(0)
+            prob = torch.sigmoid(output).squeeze(0)  # Using sigmoid for binary classification
 
-        class_1_prob = prob[1]
-        class_1_prob = class_1_prob.detach().cpu()
+        class_1_prob = prob.item()  # Convert to float
 
         return float(class_1_prob)
 
-class AutoMorphModel(nn.Module):
-    def __init__(self):
-        super(AutoMorphModel, self).__init__()
+def build_automorph_model():
+    # code taken from https://github.com/rmaphoh/AutoMorph/blob/main/M1_Retinal_Image_quality_EyePACS/model.py
+    model = EfficientNet.from_pretrained('efficientnet-b4')
+    model._fc = nn.Identity()
+    net_fl = nn.Sequential(
+        nn.Linear(1792, 256),
+        nn.ReLU(),
+        nn.Dropout(p=0.5),
+        nn.Linear(256, 64),
+        nn.ReLU(),
+        nn.Dropout(p=0.5),
+        nn.Linear(64, 3)
+    )
+    model._fc = net_fl
+    # add a final layer that outputs single value
+    model._fc.add_module("7", nn.Linear(3, 1))
+    return model
 
-        # code taken from https://github.com/rmaphoh/AutoMorph/blob/main/M1_Retinal_Image_quality_EyePACS/model.py
-        self.model = EfficientNet.from_pretrained('efficientnet-b4')
-        self.model._fc = nn.Identity()
-        net_fl = nn.Sequential(
-            nn.Linear(1792, 256),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(256, 64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(64, 3)
-        )
-        self.model._fc = net_fl
-        # add a final layer that outputs single value
-        self.model._fc.add_module("7", nn.Linear(3, 1))
+
+def remove_prefix_in_state_dict(state_dict, prefix):
+    """
+    Remove the prefix from state_dict keys.
+    """
+    return {key[len(prefix):]: value for key, value in state_dict.items() if key.startswith(prefix)}
